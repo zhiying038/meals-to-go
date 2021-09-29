fd410037225cb8125f4cff2f319649b337b04393
Yoh Zhi Ying <zhiying038@gmail.com>
2021-09-29 23:30:22 +0800

    fix navigation after auth

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,46 +1,15 @@
 import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { AuthenticationContext } from "contexts/AuthenticationContext";
 import { AuthNavigator } from "./AuthNavigator";
 import { TabNavigator } from "./TabNavigator";
 
-type StackList = {
-  auth: undefined;
-  app: undefined;
-};
-
-const Stack = createStackNavigator<StackList>();
-
-const MainStack = () => {
+export const RootNavigator = () => {
   const { isAuthenticated } = useContext(AuthenticationContext);
 
   return (
-    <Stack.Navigator
-      initialRouteName={!isAuthenticated ? "auth" : "app"}
-      screenOptions={{
-        headerShown: false,
-        gestureEnabled: true,
-      }}
-    >
-      <Stack.Screen
-        name="auth"
-        options={{ headerShown: false }}
-        component={AuthNavigator}
-      />
-      <Stack.Screen
-        name="app"
-        options={{ headerShown: false }}
-        component={TabNavigator}
-      />
-    </Stack.Navigator>
-  );
-};
-
-export const RootNavigator = () => {
-  return (
     <NavigationContainer>
-      <MainStack />
+      {isAuthenticated ? <TabNavigator /> : <AuthNavigator />}
     </NavigationContainer>
   );
 };
